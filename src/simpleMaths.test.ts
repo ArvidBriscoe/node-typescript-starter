a674afb99c0cbbf07cb5f374642d0ea6222c6f2a
Year 12 <year12@mar-admins-MacBook-Air-3.local>
2021-11-25 09:02:13 +0000

    added comparitor and test

--- src/simpleMaths.test.ts
+++ src/simpleMaths.test.ts
@@ -1,4 +1,4 @@
-import { calcAreaSquare, calcAreaTriangle } from './simpleMaths'
+import { calcAreaSquare, calcAreaTriangle, comparitor } from './simpleMaths'
 
 test('Area Square', () => {
     expect(calcAreaSquare(4)).toBe(16);
@@ -6,4 +6,8 @@ test('Area Square', () => {
 
 test('Area Triangle', () => {
     expect(calcAreaTriangle(4, 5)).toBe(10);
+})
+
+test('Comparitor', () => {
+    expect(comparitor(2, 7)).toBe(7);
 })
--- src/simpleMaths.ts
+++ src/simpleMaths.ts
@@ -6,3 +6,15 @@ export function calcAreaTriangle(width: number, height: number): number {
     return width * height / 2;
 }
 
+export function comparitor(a: number, b: number): number {
+    if (a > b) {
+      return a
+    }
+    else if (a = b) {
+      return a
+    }
+    else {
+      return b
+    }
+  }
+  
